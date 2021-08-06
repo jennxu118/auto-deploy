33a39bc6b3bb7d9687ee1312937abbfe01f4e46e
Xu, Jennifer <JenniferXu@rockethomes.com>
2021-08-06 18:38:39 -0400

    Update new-release-version.sh

--- scripts/new-release-version.sh
+++ scripts/new-release-version.sh
@@ -57,7 +57,9 @@ fi
 
 # Validate current version
 rx='^([0-9]+\.){0,2}(\*|[0-9]+)$'
-if ! [[ $version =~ $rx ]]; then
+if [[ $version =~ $rx ]]; then
+  $validate=true
+else
  echo "ERROR:<->invalidated version: '$version'"
  exit 1
 fi
